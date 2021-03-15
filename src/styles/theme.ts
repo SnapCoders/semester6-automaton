cca69d6373e114cbc88518d13ed48ef8c72dc883
Bruno Futema <bruno.futema@outlook.com>
2021-03-15 01:33:39 -0300

    style: :art: Add Theme object to provide to app

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background:
+      'radial-gradient(circle farthest-corner at center, #3C4B57 0%, #1C262B 100%)',
+    color: '#fbfbfb',
+  },
+};
+
+export default theme;
