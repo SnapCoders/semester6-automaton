2f78e1ca0b6e8a5fc86075be3c5572db186255aa
Bruno Futema <bruno.futema@outlook.com>
2021-03-16 00:12:50 -0300

    feat: :sparkles: Add Grid quark component

---
+++ src/components/quarks/Grid/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Grid: React.FC = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default Grid;
