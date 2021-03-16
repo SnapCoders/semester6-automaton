a7a1c5f1f668a613318a4fb0d05f116ff9b613f4
Bruno Futema <bruno.futema@outlook.com>
2021-03-16 00:16:27 -0300

    changes: :wrench: Changing automaton example

--- src/components/molecules/Automaton/index.tsx
+++ src/components/molecules/Automaton/index.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 
 import { State, Transition } from '../../atoms';
+import { Grid } from '../../quarks';
 
 import { Container } from './styles';
 
 const Automaton: React.FC = () => {
   return (
     <Container>
-      <State label="s" isActive isInitial>
-        <Transition label="a" transitionAs="loop" />
-        <Transition label="b" transitionAs="rightToLeft" />
-      </State>
+      <Grid>
+        <State label="1" isActive isInitial>
+          <Transition label="A" transitionAs="loop" />
+          <Transition label="B" transitionAs="leftToRight" />
+          <Transition label="C" transitionAs="upToDown" />
+        </State>
 
-      <Transition label="a" transitionAs="leftToRight" />
+        <State label="2">
+          <Transition label="B" transitionAs="leftToRight" />
+          <Transition label="A" transitionAs="rightToLeft" />
+        </State>
 
-      <State label="x">
-        <Transition label="a" transitionAs="rightToLeft" />
-      </State>
+        <State label="3">
+          <Transition label="C" transitionAs="rightToLeft" />
+        </State>
 
-      <Transition label="a" transitionAs="leftToRight" />
+        <State label="4">
+          <Transition label="B" transitionAs="downToUp" />
+          <Transition label="A" transitionAs="leftToRight" />
+        </State>
 
-      <State label="t" isFinal />
+        <State label="5">
+          <Transition label="B" transitionAs="rightToLeft" />
+          <Transition label="C" transitionAs="leftToRight" />
+        </State>
+
+        <State label="6">
+          <Transition label="B" transitionAs="rightToLeft" />
+        </State>
+      </Grid>
     </Container>
   );
 };
